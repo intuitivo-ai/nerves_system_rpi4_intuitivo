d90526f3a98976d8ca99cd43090d9d2281b080ca
Jose Rodriguez <jose.rodriguez@intuitivo.com>
2026-03-30 19:39:31 -0300

    version gg

--- package/greengrass-lite/greengrass-lite.mk
+++ package/greengrass-lite/greengrass-lite.mk
@@ -2,9 +2,13 @@
 #
 # greengrass-lite
 #
+# Upstream minimum deps: aws-greengrass-lite docs/SETUP.md
+# Buildroot 2024.02.x (typical): curl 8.x, openssl 3.x, sqlite 3.44.x,
+# libevent 2.1.12, libyaml/libzip/uriparser/util-linux above mins, libcgroup 2.0.3+.
+# CMake enforces libcurl>=7.68 via pkg-config; other libs via pkg_search_module.
 ################################################################################
 
-GREENGRASS_LITE_VERSION = v2.3.3
+GREENGRASS_LITE_VERSION = v2.4.0
 GREENGRASS_LITE_SITE = $(call github,aws-greengrass,aws-greengrass-lite,$(GREENGRASS_LITE_VERSION))
 GREENGRASS_LITE_LICENSE = Apache-2.0
 GREENGRASS_LITE_LICENSE_FILES = LICENSE
@@ -20,6 +24,7 @@ GREENGRASS_LITE_DEPENDENCIES = \
 	libzip \
 	util-linux \
 	openssl \
+	libcgroup \
 	host-pkgconf
 
 GREENGRASS_LITE_CONF_OPTS = \
